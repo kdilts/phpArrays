5a54a5230f7265773519678d2ec10103e31fe665
Kevin Dilts <kdilts13@gmail.com>
2016-09-19 17:09:43 -0600

    Added examples of creating a normal array and an associative array.

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
 	<body>
 		<?php
 		echo "<h1>PHP Arrays</h1>";
+
+		$test[] = "item 1";
+		$test[] = "item 2";
+		$test[] = "item 3";
+		$test[] = "item 4";
+		echo '$test = ';
+		print_r($test);
+
+		echo "<br>";
+
+		$assoc['item 1'] = 'one';
+		$assoc['item 2'] = 'two';
+		$assoc['item 3'] = 'three';
+		echo '$assoc = ';
+		print_r($assoc);
+
+		echo "<br>";
+
+
 		?>
 	</body>
 </html>
