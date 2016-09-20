86e39f37a5638470eeffebce1d169f8eb0ea7dd0
Kevin Dilts <kdilts13@gmail.com>
2016-09-20 13:17:20 -0600

    Added examples of creating a normal array and an associative array using the array() keyword

--- index.php
+++ index.php
@@ -6,23 +6,34 @@
 		<?php
 		echo "<h1>PHP Arrays</h1>";
 
+		// regular array
 		$test[] = "item 1";
 		$test[] = "item 2";
 		$test[] = "item 3";
 		$test[] = "item 4";
 		echo '$test = ';
 		print_r($test);
-
 		echo "<br>";
 
+		// associative array
 		$assoc['item 1'] = 'one';
 		$assoc['item 2'] = 'two';
 		$assoc['item 3'] = 'three';
 		echo '$assoc = ';
 		print_r($assoc);
-
 		echo "<br>";
 
+		// array keyword - regular array
+		$arr = array('a','b','c');
+		echo '$arr = ';
+		print_r($arr);
+		echo "<br>";
+
+		// array keyword - associative array
+		$assoc2 = array('1'=>'a', '2'=>'b', '3'=>'c');
+		echo '$assoc2 = ';
+		print_r($assoc2);
+		echo "<br>";
 
 		?>
 	</body>
